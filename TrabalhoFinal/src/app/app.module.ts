20cac36bfd4eef2a380a71162fda8f6a0cd0ee87
Emerson00Santos <emersonsantos.cep@gmail.com>
2020-12-05 18:49:07 -0300

    Correção Model

--- TrabalhoFinal/src/app/app.module.ts
+++ TrabalhoFinal/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioRegistroComponent } from './usuario-registro/usuario-registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioRegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TrabalhoFinal/src/app/usuario-registro/usuario-registro.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from "../usuario";
+
+
+@Component({
+    selector: 'app-usuario-registro',
+    templateUrl: './usuario-registro.component.html',
+    styleUrls: ['./usuario-registro.component.css']
+})
+export class UsuarioRegistroComponent implements OnInit {
+
+    constructor() { }
+
+    usuario: Usuario;
+    nomeRegistro: string;
+    emailRegistro: string;
+    senhaRegistro: string;
+
+
+
+    usuarioModel = new Usuario('','','');
+    ngOnInit(): void {
+    }
+
+    onSubmit() {
+
+        let nome_registro = this.usuarioModel.nome;
+        let email_registro = this.usuarioModel.email;
+        let senha_registro = this.usuarioModel.senha;
+
+        this.usuario = new Usuario(nome_registro, email_registro, senha_registro);
+
+        console.log(this.usuario);
+    }
+
+}
