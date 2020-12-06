5a404acbc1155392296598cb52f47b2d0986d3e8
Ygor Carvalho <ygcarvalh@gmail.com>
2020-12-06 18:14:49 -0300

    Commit 2 06/12/2020

--- TrabalhoFinal/src/app/app-routing.module.ts
+++ TrabalhoFinal/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { jogosRegistroComponent } from './jogos-registro/jogos-registro.component';
 import { UsuarioRegistroComponent } from './usuario-registro/usuario-registro.component';
 import { UsuarioLoginComponent } from './usuario-login/usuario-login.component';
+import { ListaReviewsComponent } from './lista-reviews/lista-reviews.component';
 
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'usuario-registro', component: UsuarioRegistroComponent },
   { path: 'usuario-login', component:  UsuarioLoginComponent },
   { path: 'jogo-registro', component: jogosRegistroComponent },
+  { path: 'lista-reviews', component: ListaReviewsComponent }
 
 ];
 
