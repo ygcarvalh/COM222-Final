9437eff4905ab4e623dc931905190977be1be410
Robert_Mendes <51209840+robertnicolas88@users.noreply.github.com>
2020-12-07 20:37:02 -0300

    Arrumei - app-routing-modules

--- TrabalhoFinal/src/app/app-routing.module.ts
+++ TrabalhoFinal/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { UsuarioRegistroComponent } from './usuario-registro/usuario-registro.co
 import { UsuarioLoginComponent } from './usuario-login/usuario-login.component';
 import { ListaReviewsComponent } from './lista-reviews/lista-reviews.component';
 import { CriarReviewComponent } from './criar-review/criar-review.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { ListaJogosComponent } from './lista-jogos/lista-jogos.component';
 
 
 const routes: Routes = [
-	{ path: '', component: MenuPrincipalComponent },
+	{ path: 'menu-principal', component: MenuPrincipalComponent },
 	{ path: 'usuario-registro', component: UsuarioRegistroComponent },
 	{ path: 'usuario-login', component: UsuarioLoginComponent },
 	{ path: 'jogo-registro', component: jogosRegistroComponent },
